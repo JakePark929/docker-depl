d6536db156396c8647d2d73afa1316c00196c309
epike <epikeran@gmail.com>
2023-03-30 17:13:44 +0900

    init: initialize commit

---
+++ src/main/java/com/jake/dockerdepl/DockerdeplApplication.java
@@ -0,0 +1,13 @@
+package com.jake.dockerdepl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerdeplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerdeplApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jake/dockerdepl/DockerdeplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jake.dockerdepl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerdeplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
