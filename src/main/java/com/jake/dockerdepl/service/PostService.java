76a4a06557f3659d855a5d5c05da46051cbc5158
epike <epikeran@gmail.com>
2023-03-31 12:58:05 +0900

    chore: Add docker compose file to deploy on server

--- src/main/java/com/jake/dockerdepl/service/PostService.java
+++ src/main/java/com/jake/dockerdepl/service/PostService.java
@@ -12,6 +12,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @Slf4j
 @RequiredArgsConstructor
 @Service
